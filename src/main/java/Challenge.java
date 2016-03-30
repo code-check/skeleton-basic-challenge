895507869b2864813da2f71ea3ae1476b20b858e
sukeshni <sukeshni.kantrod@gmail.com>
2016-03-30 16:41:45 +0900

    First Commit - Java skeleton for codecheck challenge

---
+++ src/main/java/Challenge.java
@@ -0,0 +1,8 @@
+public class Challenge {
+
+  public static Integer solveChallenge(int data) {
+    //BEGIN_CHALLENGE
+    return data;
+    //END_CHALLENGE
+  }
+}
---
+++ src/test/java/TestChallenge.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestChallenge {
+
+  @Test
+  public void testChallenge() {
+    assertTrue(Challenge.solveChallenge(1) == 1);
+  }
+}
